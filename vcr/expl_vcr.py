cb637cb727e2c44398a77af78d5ba65f6d7ab28d
jules-samaran <jules.samaran@mines-paristech.fr>
2020-04-20 17:41:08 +0900

    update vcr exploring script

--- vcr/expl_vcr.py
+++ vcr/expl_vcr.py
@@ -2,6 +2,9 @@ import os
 import pprint
 import shutil
 import inspect
+import yaml
+from easydict import EasyDict as edict
+import pdb
 
 from tensorboardX import SummaryWriter
 import numpy as np
@@ -10,7 +13,11 @@ import torch.nn
 import torch.optim as optim
 import torch.distributed as distributed
 from torch.nn.parallel import DistributedDataParallel as DDP
+import sys
 
+root_path = os.path.abspath(os.getcwd())
+if root_path not in sys.path:
+    sys.path.append(root_path)
 from common.utils.create_logger import create_logger
 from common.utils.misc import summary_parameters, bn_fp16_half_eval
 from common.utils.load import smart_resume, smart_partial_load_model_state_dict
@@ -25,15 +32,19 @@ from common.nlp.bert.optimization import AdamW, WarmupLinearSchedule
 from vcr.data.build import make_dataloader, build_dataset, build_transforms
 from vcr.modules import *
 from vcr.function.val import do_validation
-import yaml
 
-with open("/Users/jules-samaran/PycharmProjects/VL-BERT/cfgs/vcr/base_q2a_4x16G_fp32.yaml", "r") as file:
-    config = yaml.load(file)
-    model = eval(config.MODULE)(config)
+import argparse
+import subprocess
 
-    train_loader = make_dataloader(config, mode='train', distributed=False)
+from vcr.function.config import config, update_config
+cfg_path = os.path.join(os.getcwd(), "cfgs/vcr/base_q2a_4x16G_fp32.yaml")
+update_config(cfg_path)
+model = eval(config.MODULE)(config)
+train_loader = make_dataloader(config, mode='train', distributed=False)
 
-    model.train()
-    # training
-    for nbatch, batch in enumerate(train_loader):
-        outputs, loss = model(*batch)
+model.train()
+# training
+
+for nbatch, batch in enumerate(train_loader):
+    outputs, loss = model(*batch)
+
