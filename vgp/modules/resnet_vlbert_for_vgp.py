cfd6cfbbbfd5a10edca8f9548c4ab7dcff7394e6
jules-samaran <jules.samaran@mines-paristech.fr>
2020-05-11 19:16:07 +0900

    fix mask to handle alignment for positive pairs

--- vgp/modules/resnet_vlbert_for_vgp.py
+++ vgp/modules/resnet_vlbert_for_vgp.py
@@ -288,8 +288,8 @@ class ResNetVLBERT(Module):
             if sentence_label.sum() == sentence_label.shape[0]:  # Loss is 0 when all samples are positive
                 alignment_loss = torch.tensor([0], dtype=torch.float32)
             else:
-                alignment_loss = F.binary_cross_entropy_with_logits(alignment_logits[1 - sentence_label],
-                                                                    first_correct[1 - sentence_label].type(torch.float32))
+                alignment_loss = F.binary_cross_entropy_with_logits(alignment_logits[sentence_label == 0],
+                                                                    first_correct[sentence_label == 0].type(torch.float32))
             outputs.update({'alignment_logits': alignment_logits,
                             'alignment_label': first_correct,
                             'alignment_loss': alignment_loss})
