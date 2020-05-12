0811418b6e07c6e3574aabad85559d07673a1cd4
jules-samaran <jules.samaran@mines-paristech.fr>
2020-05-12 15:50:33 +0900

    Fix vqa config and validation script

--- vqa/function/val.py
+++ vqa/function/val.py
@@ -13,6 +13,5 @@ def do_validation(net, val_loader, metrics, label_index_in_batch):
         datas = [batch[i] for i in range(len(batch)) if i != label_index_in_batch % len(batch)]
 
         outputs = net(*datas)
-        outputs.update({'sentence_label': label})
+        outputs.update({'label': label})
         metrics.update(outputs)
-
