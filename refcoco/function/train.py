6dbd51686148134a761e7b8cd793df65c129e91c
Vladimir Valeyev <ValV@users.noreply.github.com>
2023-05-11 03:13:53 +0300

    Fix imports

--- refcoco/function/train.py
+++ refcoco/function/train.py
@@ -3,35 +3,41 @@ import pprint
 import shutil
 import inspect
 
-from tensorboardX import SummaryWriter
 import numpy as np
 import torch
 import torch.nn
 import torch.optim as optim
 import torch.distributed as distributed
+
+from tensorboardX import SummaryWriter
 from torch.nn.parallel import DistributedDataParallel as DDP
 
 from common.utils.create_logger import create_logger
 from common.utils.misc import summary_parameters, bn_fp16_half_eval
 from common.utils.load import smart_resume, smart_partial_load_model_state_dict
+
 from common.trainer import train
+
 from common.metrics.composite_eval_metric import CompositeEvalMetric
 from common.metrics import refcoco_metrics
+
 from common.callbacks.batch_end_callbacks.speedometer import Speedometer
 from common.callbacks.epoch_end_callbacks.validation_monitor import ValidationMonitor
 from common.callbacks.epoch_end_callbacks.checkpoint import Checkpoint
+
 from common.lr_scheduler import WarmupMultiStepLR
+
 from common.nlp.bert.optimization import AdamW, WarmupLinearSchedule
+
 from refcoco.data.build import make_dataloader, build_dataset, build_transforms
-# from refcoco.modules import *
 from refcoco.function.val import do_validation
+from refcoco.modules import *
 
 try:
     from apex import amp
     from apex.parallel import DistributedDataParallel as Apex_DDP
 except ImportError:
     pass
-    # raise ImportError("Please install apex from https://www.github.com/nvidia/apex if you want to use fp16.")
 
 
 CUDA_AVAILABLE = torch.cuda.is_available()
